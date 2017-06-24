417b83c496390b653c92aac2ccdb758ba89336b5
Alex Sanford <alex.sanford1@gmail.com>
2017-06-24 14:32:56 -0300

    Set up AggressiveSplittingPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 var path = require( 'path' );
+var webpack = require( 'webpack' );
 
 module.exports = {
 	entry: './index.js',
 	output: {
 		filename: 'bundle.js',
+		chunkFilename: '[chunkhash].js',
 		path: path.resolve( __dirname, 'dist' ),
 		publicPath: 'dist/'
-	}
+	},
+	plugins: [
+		new webpack.optimize.AggressiveSplittingPlugin( {
+			minSize: 30000,
+			maxSize: 50000
+		} )
+	]
 }
